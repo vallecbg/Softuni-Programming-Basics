3bb55b2959e5ff693af98a0d7da4c2053545f923
Valentin Trifonov <valioplayofficial@gmail.com>
2018-01-11 23:37:41 +0200

    First Steps in Coding

---
+++ First Steps in Coding/03. Nums 1...20/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Nums_1._._._20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 1; x <= 20; x++)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
---
+++ First Steps in Coding/06. Square of Stars/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Square_of_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = 1; j <= N; j++)
+                {
+                    if ((i != 1) && (i != N) && (j != 1) && (j != N))
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
